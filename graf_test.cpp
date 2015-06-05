9fc2c66320720a8f4a3a64131ca94d0b3ad6129f
milleniumbug <wojas1222@gmail.com>
2015-06-05 02:54:46 +0200

    poprawiono pomyłkę w testach

--- graf_test.cpp
+++ graf_test.cpp
@@ -229,8 +229,9 @@ void testuj_kolejne(unsigned int seed)
 	std::cout << "\n";
 	test_wiele_klik(seed, threshold, 1, "out1.gv");
 	test_wiele_klik(seed, threshold, 5, "out2.gv");
-	test_wikipedia(seed, threshold, false, "out3.gv");
-	test_wikipedia(seed, threshold, true, "out3.gv");
+	test_wiele_klik(seed, threshold, 50, "out3.gv");
+	test_wikipedia(seed, threshold, false, "out4.gv");
+	test_wikipedia(seed, threshold, true, "out5.gv");
 }
 
 void testuj_kolejne()
