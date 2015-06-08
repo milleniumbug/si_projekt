347d1df4e6d7d4a464dc3d2d75eba1a8311aea9c
milleniumbug <wojas1222@gmail.com>
2015-06-08 16:06:02 +0200

    funkcja na zwiększanie z saturacja

--- utils.h
+++ utils.h
@@ -8,6 +8,11 @@
 // zwraca star¹ wartoœæ `value` przed inkrementacj¹
 double interlocked_increase(std::atomic<double>& value, double what);
 
+// interlocked_increase_with_saturation
+// atomowo albo zwiêksza `value` o `what` albo ustawia `value` na `max`, w zale¿noœci od tego która jest wiêksza
+// zwraca star¹ wartoœæ `value` przed inkrementacj¹
+double interlocked_increase_with_saturation(std::atomic<double>& value, double max, double what);
+
 template<typename Zbior, typename Element>
 bool jest_w_zbiorze(const Zbior& zb, const Element& el)
 {
